fc857142e01ab689aec6c3dfd5c65e226b9d37d7
AlexR <aleeexrp02@gmail.com>
2022-10-16 17:29:25 +0200

    ex04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let arraySolution = [] ;
+    arraySolution = array.filter(x => x!==args[0]) ;
+    for (let i = 1; i < args.length; i++) {
+        arraySolution = arraySolution.filter(x => x!==args[i]) ;
+    }
+    return arraySolution;
 };
 
 // Do not edit below this line
