28b046b5f41ab10a9740f87778b0c9afcac70ccf
AlexR <aleeexrp02@gmail.com>
2022-10-16 17:19:06 +0200

    ex3 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let solution = '' ;
+    for (let i = string.length; i >= 0; i--) {
+        solution += string.charAt(i) ;
+    }
+    return solution ;
 };
 
 // Do not edit below this line
