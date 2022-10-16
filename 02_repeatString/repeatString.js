65754530da958d79916145dacd091bb7ebc1a333
AlexR <aleeexrp02@gmail.com>
2022-10-16 17:13:57 +0200

    ex2 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return ('ERROR') ;
+    }
+    let newstring = '' ;
+    for (let i = 0; i  < number; i++) {
+        newstring += string ;
+    }
+    return newstring ;
 };
 
 // Do not edit below this line
