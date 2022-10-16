cffd72242cb34039ca30153abdfb59d1e40eb8cb
AlexR <aleeexrp02@gmail.com>
2022-10-16 17:50:43 +0200

    ex07 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ctof = function(degrees) {
+  let solution = 0 ;
+  solution = degrees * 9 / 5 + 32;
+  solution = Math.round(solution*10)/10 ;
+  return (solution) ;
 };
 
-const ctof = function() {
-
+const ftoc = function(degrees) {
+  let solution = 0 ;
+  solution = (degrees-32) * 5 / 9 ;
+  solution = Math.round(solution*10)/10 ;
+  return (solution) ;
 };
 
 // Do not edit below this line
