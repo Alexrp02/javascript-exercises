05fc1085109cf5315cd3dba08de07771c50390c9
AlexR <aleeexrp02@gmail.com>
2022-10-16 17:59:48 +0200

    ex08 solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1+num2) ;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1-num2) ;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let solution = 0 ;
+	array.forEach(element => {
+    solution += element ;
+  });
+  return solution ;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let solution = 1 ;
+  array.forEach(element => {
+    solution*=element ;
+  }
+    ) ;
+    return solution ;
 };
 
-const power = function() {
-	
+const power = function(number, pow) {
+  let solution = 1 ;
+  for (let i = 0; i < pow; i++) {
+    solution *= number ;
+  }
+	return (solution) ;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+  let solution = 1 ;
+  for (let i = 1; i <= fact; i++) {
+    solution *= i ; 
+  }
+  return solution ;
 };
 
 // Do not edit below this line
