a3fca78038392fc2c7ccbffe0da82361cedfc7a6
AlexR <aleeexrp02@gmail.com>
2022-10-17 11:24:53 +0200

    ex09 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let wordReversed = reverseStringWithout(word).replace(/\W|_/g, '');
+    let wordWithoutPunct = word.replace(/\W|_/g, '');
+    console.log(wordReversed) ;
+    console.log(wordWithoutPunct) ;
+    let solution;
+    wordReversed.toLowerCase() == wordWithoutPunct.toLowerCase() ? solution=true : solution=false ;
 
+    return solution ;
 };
+
+const reverseStringWithout = function(string) {
+    let solution = '' ;
+    for (let i = string.length; i >= 0; i--) {    
+        solution += string.charAt(i) ;
+    }
+    return solution ;
+};
+
+palindromes('racecar!') ;
 
 // Do not edit below this line
 module.exports = palindromes;
