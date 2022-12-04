45b0f507ef47e75850d3e3ab7a22ff5657339527
AlexR <aleeexrp02@gmail.com>
2022-12-04 19:14:24 +0100

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+let sol = 0 ;
+let val ;
+let prev ;
+number = Number(number) ;
+if (number < 0) {
+    return "OOPS" ;
+}else if (number == 1 ) {
+    sol = 1 ;
+}else if (number > 1 ) {
+    prev = 0 ;
+    val = 1 ;
+    sol = 1 ;
+    for(let i=2 ; i<=number ; i++) {
+        sol = prev+val ;
+        prev = val ;
+        val = sol ;
+    }
+}
 
+return sol ;
 };
 
 // Do not edit below this line
