c67dd16f4c756882f5c97414244b7c9e319e7c0c
AlexR <aleeexrp02@gmail.com>
2022-12-04 19:46:16 +0100

    findTheOldest solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date ;
+    let sol = people.reduce((accumulator, actual) => {
+        if (!actual.yearOfDeath)
+            actual.yearOfDeath = date.getFullYear() ;
+        if (!accumulator.yearOfDeath)
+            accumulator.yearOfDeath = date.getFullYear() ;
+        if ((actual.yearOfDeath-actual.yearOfBirth) >= (accumulator.yearOfDeath-accumulator.yearOfBirth)) 
+            accumulator = actual ;
+        return accumulator ;
+    }, people[0]) ;
+    return sol ;
+    
 };
 
 // Do not edit below this line
